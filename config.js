9f88fc747f40def2b1a64ef01e9d590f74d4dab2
eden-gu <86941608+eden-gu@users.noreply.github.com>
2021-08-05 17:07:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "点我有惊喜！",
+        play: "等下，有点枯燥，来点音乐怎么样？",
+        bannar_coming: "来点装饰呀！",
+        balloons_flying: "好像还是少了点东西",
+        cake_fadein: "蛋糕整一个！",
+        light_candle: "蜡烛给点上！",
+        wish_message: "生日快乐！",
         story: "A MESSAGE FOR YOU",
     }
 };
